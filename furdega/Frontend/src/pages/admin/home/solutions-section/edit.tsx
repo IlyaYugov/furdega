050fa83702fa95af1cbd2a33f5182c19c6ee4ea7
Viktor Skorodumov <skorodumov.v@medindex.ru>
2021-10-05 17:37:32 +0300

    front

--- furdega/Frontend/src/pages/admin/home/solutions-section/edit.tsx
+++ furdega/Frontend/src/pages/admin/home/solutions-section/edit.tsx
@@ -5,12 +5,14 @@ import { v4 as uuidv4 } from "uuid"
 
 import { AdminSectionMode } from "../../../../const/admin"
 import { ReactComponent as YellowSnakeIcon } from "../../../../assets/svg/yellow-snake.svg"
-import { FormInputEvent } from "../../../../types/utils"
 import { SolutionEdit } from "./solution-edit"
 import {
   IssueSolutionResponse,
   IssueSolutionsSectionResponse,
+  IssueSolutionsSectionCreateRequest,
+  IssueSolutionsSectionUpdateRequest,
 } from "../../../../types/home/solutions"
+import { issueSolutionsSectionApi } from "../../../../api/home/issue-solutions-section-api"
 
 type EditProps = {
   data: IssueSolutionsSectionResponse
@@ -52,8 +54,87 @@ const Edit: FC<EditProps> = ({ data, setMode }) => {
   )
 
   const save = async () => {
-    // isDataEmpty
-    console.log()
+    if (isDataEmpty) {
+      if (
+        !(
+          newImagesBase64.solution1 &&
+          newImagesBase64.solution2 &&
+          newImagesBase64.solution3 &&
+          newImagesBase64.solution4
+        )
+      )
+        return
+
+      const request: IssueSolutionsSectionCreateRequest = {
+        header,
+        issueSolution1: {
+          ...solution1,
+          image: {
+            id: solution1.image.id,
+            base64ImageString: newImagesBase64.solution1,
+          },
+        },
+        issueSolution2: {
+          ...solution2,
+          image: {
+            id: solution2.image.id,
+            base64ImageString: newImagesBase64.solution2,
+          },
+        },
+        issueSolution3: {
+          ...solution3,
+          image: {
+            id: solution3.image.id,
+            base64ImageString: newImagesBase64.solution3,
+          },
+        },
+        issueSolution4: {
+          ...solution4,
+          image: {
+            id: solution4.image.id,
+            base64ImageString: newImagesBase64.solution4,
+          },
+        },
+      }
+
+      await issueSolutionsSectionApi.create(request)
+    } else {
+      const request: IssueSolutionsSectionUpdateRequest = {
+        header,
+        issueSolution1: {
+          ...solution1,
+          image: {
+            id: solution1.image.id,
+            base64ImageString: newImagesBase64.solution1 || null,
+          },
+        },
+        issueSolution2: {
+          ...solution2,
+          image: {
+            id: solution2.image.id,
+            base64ImageString: newImagesBase64.solution2 || null,
+          },
+        },
+        issueSolution3: {
+          ...solution3,
+          image: {
+            id: solution3.image.id,
+            base64ImageString: newImagesBase64.solution3 || null,
+          },
+        },
+        issueSolution4: {
+          ...solution4,
+          image: {
+            id: solution4.image.id,
+            base64ImageString: newImagesBase64.solution4 || null,
+          },
+        },
+      }
+
+      await issueSolutionsSectionApi.update(request)
+    }
+
+    setMode(AdminSectionMode.view)
   }
 
   const onSolution1Change = (
