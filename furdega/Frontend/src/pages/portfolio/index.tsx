9af9784408762280213d35ab58731863386adaf8
Viktor Skorodumov <skorodumoviktor@gmail.com>
2021-11-19 20:46:50 +0300

    12

--- furdega/Frontend/src/pages/portfolio/index.tsx
+++ furdega/Frontend/src/pages/portfolio/index.tsx
@@ -1,5 +1,5 @@
 import { Col, Container, Row } from "react-bootstrap"
-import { FC, useEffect, useState } from "react"
+import { FC, useEffect, useRef, useState } from "react"
 
 import { furnitureTypesApi } from "../../api/furniture-types-api"
 import { Furniture, FurnitureType } from "../../types/furniture"
@@ -19,6 +19,7 @@ const Portfolio: FC = () => {
     useState<FurnitureType>(defaultFurType)
   const [furnitureTypes, setFurnitureTypes] = useState<FurnitureType[]>([])
   const [furnitures, setFurnitures] = useState<Furniture[]>([])
+  const topRef = useRef<HTMLDivElement>(null)
 
   const fetchFurnitureTypes = async () => {
     const data = await furnitureTypesApi.get()
@@ -33,6 +34,10 @@ const Portfolio: FC = () => {
   const fetchFurnituresByTypeId = async (typeId: number) => {
     const data = await furnituresApi.getByFurnitureTypeId(typeId)
     setFurnitures(data)
+  }
+
+  const scrollToTop = () => {
+    topRef?.current?.scrollIntoView({ behavior: "smooth" })
   }
 
   useEffect(() => {
@@ -52,7 +57,7 @@ const Portfolio: FC = () => {
     <Container fluid className="g-0">
       <PageHeader imageSrc="/assets/portfolio-top-pic.jpg" title="Портфолио" />
 
-      <Container className="g-0 content overflow-hidden">
+      <Container className="g-0 content">
         <Row className="flex-nowrap py-5 g-0 bg-light">
           <Col className="scrollspy-col" sm={4} md={4} lg={3}>
             <Nav
@@ -60,16 +65,24 @@ const Portfolio: FC = () => {
               activeItem={furnitureType}
               onItemClick={(type) => {
                 setFurnitureType(type)
+                scrollToTop()
               }}
             />
           </Col>
 
-          <Col xs={12} sm={8} md={8} lg={9} className="px-3 ps-sm-5">
+          <Col
+            ref={topRef}
+            xs={12}
+            sm={8}
+            md={8}
+            lg={9}
+            className="px-3 ps-sm-5 overflow-hidden"
+          >
             <Title title={furnitureType.title} />
 
             <Row className="flex-column gy-5">
               {furnitures.map((fur, index) => (
-                <Col>
+                <Col key={fur.id}>
                   <FurView {...fur} index={index} />
                 </Col>
               ))}
